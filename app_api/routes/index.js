e1b992f0e452ec264e214f16b5015930a312fcf2
b5yB <89233920+b5yB@users.noreply.github.com>
2024-06-08 08:25:46 -0500

    module 5 completed baseline

---
+++ app_api/routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+const tripsController = require('../controllers/trips');
+
+// get all trips
+router
+    .route('/trips')
+    .get(tripsController.tripsList);
+
+// get trip by code
+router
+    .route('/trips/:tripCode')
+    .get(tripsController.tripsFindByCode);
+
+module.exports = router;
